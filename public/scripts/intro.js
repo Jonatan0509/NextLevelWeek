f8d1ab8f3494f4e9dff60898a97208c2e39519b0
Jonatan Brum <jonatanbrum@hotmail.com>
2020-08-21 16:13:19 -0300

    Versão1

---
+++ public/scripts/intro.js
@@ -0,0 +1,68 @@
+// comments
+// document.write('Hello')
+
+// variaveis, tipo de dados
+// var myvar
+// myvar = "Hi"
+// const myconst = "Hello"
+// document.write(myvar + myconst)
+
+
+// string
+// "Isso é uma string"
+// 'Isso também é uma string'
+// `Isso é uma string também`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean =  true ou false
+// const btrue = true
+// const bfalse = false
+// document.write(bfalse)
+
+// objetos possuem propriedades e funcionalidaes
+// const pessoa = {
+//   altura: "1,80cm",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("Executar uma grande lógica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array = vetores
+// const colecaoDeBolinhas = ["Blue","green", 3, {name: "My name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// Registrar função
+// function sayMyName(name) {
+
+//   document.write(name)
+// }
+
+// // executar
+// sayMyname("Jonatan")
+
+// Condicionais
+
+// const notaFinal = 7
+
+// if(notaFinal < 5) {
+//   document.write("Reprovado")
+// }
+// else {
+//   document.write("Aprovado")
+// }
+
+// Loop repetições
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
